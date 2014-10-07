4cb925c2598bfa4b95c60aa0ddc3c914bfa081cb
Suyeol Jeon <devxoul@gmail.com>
2014-10-07 15:24:31 +0900

    Update script description.

--- script/github-monospace-editor.user.js
+++ script/github-monospace-editor.user.js
@@ -2,10 +2,9 @@
 // @name       GitHub Monospace Editor
 // @namespace  http://xoul.kr/github-monospace-editor
 // @version    0.1.0
-// @description  Monospace GitHub Editor
+// @description  GitHub Monospace Editor
 // @match      github.com/*/*/*
 // @copyright  2014, devxoul
 // ==/UserScript==
 
 $(document.head).append($('<style>#pull_request_body { font-family: monospace; }</style>'));
-
